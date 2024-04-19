3522df0f094b6ca1d22e461b19e194ef4ac8c20f
Garvit Nagpal <74408151+garvit2435@users.noreply.github.com>
2024-04-19 15:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (65.09%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,49 +1,50 @@
-// class Solution {
-//     public void sortColors(int[] nums) {
-//         int n = nums.length;
-//         int zero=0, one=0, two=0;
-//         for(int i = 0; i < n; i++){
-//             if(nums[i] == 0) zero++;
-//             if(nums[i] == 1) one++;
-//             if(nums[i] == 2) two++;
-//         }
-//         for(int i = 0; i < zero; i++){
-//             nums[i] = 0;
-//         }
-//         for(int i = 0; i < one; i++){
-//             nums[zero + i] = 1;
-//         }
-//         for(int i = 0; i < two; i++){
-//             nums[zero + one + i] = 2;
-//         }
-//     }
-
-    
-// }
-//Dutch national flag algo:
+//My solution
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums, low, mid);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                swap(nums, mid, high);
-                high--;
-            }
+        int n = nums.length;
+        int zero=0, one=0, two=0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0) zero++;
+            if(nums[i] == 1) one++;
+            if(nums[i] == 2) two++;
+        }
+        for(int i = 0; i < zero; i++){
+            nums[i] = 0;
+        }
+        for(int i = 0; i < one; i++){
+            nums[zero + i] = 1;
+        }
+        for(int i = 0; i < two; i++){
+            nums[zero + one + i] = 2;
         }
     }
+
     
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
 }
+// //Dutch national flag algo:
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int low = 0, mid = 0, high = nums.length - 1;
+//         while (mid <= high) {
+//             if (nums[mid] == 0) {
+//                 swap(nums, low, mid);
+//                 low++;
+//                 mid++;
+//             } else if (nums[mid] == 1) {
+//                 mid++;
+//             } else {
+//                 swap(nums, mid, high);
+//                 high--;
+//             }
+//         }
+//     }
+    
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
 
 
 //Bubble sort
