e4fc09a9b03af9552291ed4a7e85436eac3d538e
Garvit Nagpal <74408151+garvit2435@users.noreply.github.com>
2024-04-19 14:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (79.46%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,9 +1,10 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero=0, one=0;
+        int zero=0, one=0, two=0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 0) zero++;
             if(nums[i] == 1) one++;
+            if(nums[i] == 2) two++;
         }
         for(int i = 0; i < zero; i++){
             nums[i] = 0;
@@ -11,7 +12,7 @@ class Solution {
         for(int i = 0; i < one; i++){
             nums[zero + i] = 1;
         }
-        for(int i = 0; i < (nums.length-zero-one); i++){
+        for(int i = 0; i < two; i++){
             nums[zero + one + i] = 2;
         }
     }
